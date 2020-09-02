b7616e0bc8ae93631c6d1e55fdf92b47d1d7e3f2
Himanshu Garg <hgarg701@gmail.com>
2020-09-01 18:30:21 -0700

    Variables, functions, array, dictionary

--- index.php
+++ index.php
@@ -6,7 +6,37 @@
   </head>
   <body>
     <?php
-        echo "Hello from php";
+      //Simple print statement
+      echo "TESTING PRINT STATEMENT <br />";
+        echo "Hello from php <br /><br />";
+
+
+        //Variables
+        $x = 5;
+        $y = "John";
+        $z = 3.54;
+
+        echo "TESTING VARIABLES <br />";
+        echo "Hello $y, you need to pay $$z and not $$x <br /><br />";
+
+        //array
+        echo "TESTING ARRAY<br />";
+        $fruits = array("apple", "mango", "banana", "grapes", "watermelon");
+        echo "$fruits";
+        echo "Number of fruits ".count($fruits)."<br /><br />";
+
+        echo "TESTING DICTIONARY <br />";
+        $marks = array("Aaraon"=>"A+", "Bug" => "B+", "Chris" => "A-");
+        echo "Marks obtained by Bug ".$marks["Bug"]." <br /><br />";
+
+        //Hey function
+        function hey(){
+          echo "Hello There <br /><br />";
+        }
+
+        //Simple function call to hey()
+        echo "TESTING FUNCTION <br />";
+        hey();
      ?>
 
   </body>
